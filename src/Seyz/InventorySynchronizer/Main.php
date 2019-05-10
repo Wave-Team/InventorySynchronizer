f8f3e234f0cca1c40ff8059913248c7566a92b65
Seyz123 <seylio@outlook.com>
2019-05-10 22:57:12 +0200

    Upload files

---
+++ src/Seyz/InventorySynchronizer/Main.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Seyz\InventorySynchronizer;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\event\{
+	Listener,
+	player\PlayerLoginEvent,
+	player\PlayerQuitEvent
+};
+
+class Main extends PluginBase implements Listener {
+
+	private $config;
+	private static $instance;
+
+	public function onLoad()
+	{
+		self::$instance = $this;
+	}
+
+	public function onEnable()
+	{
+		@mkdir($this->getDataFolder());
+		if(!file_exists($this->getDataFolder()."config.yml")) $this->saveResource("config.yml");
+		$this->config = new Config($this->getDataFolder()."config.yml", Config::YAML);
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		Database::init();
+	}
+
+	public function onLogin(PlayerLoginEvent $ev)
+	{
+		$player = $ev->getPlayer();
+
+		if(!Database::isRegistered($player)){
+			Database::register($player);
+		} else {
+			Database::restoreInventory($player);
+		}
+	}
+
+	public function onQuit(PlayerQuitEvent $ev)
+	{
+		$player = $ev->getPlayer();
+
+		if(Database::isRegistered($player)){
+			Database::saveInventory($player);
+		}
+	}
+
+	public function getConfig() : Config
+	{
+		return $this->config;
+	}
+
+	public static function getInstance()
+	{
+		return self::$instance;
+	}
+}
